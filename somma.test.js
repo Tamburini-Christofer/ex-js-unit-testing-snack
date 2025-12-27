395bc76a2f4d9fb689649dfaeb1239d6b63ddcbc
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2025-12-27 11:18:01 +0100

    Snack 4

--- somma.test.js
+++ somma.test.js
@@ -1,30 +1,34 @@
+// Snack 1
 function getInitials(fullName) {
-  const names = fullName.split(" ");
-  let initials = "";
-  for (let i = 0; i < names.length; i++) {
-    initials += names[i].charAt(0).toUpperCase();
-  }
-  return initials;
+  const [name, fName] = fullName.split(" ");
+  return `${name.charAt(0).toUpperCase()}${fName.charAt(0).toUpperCase()}`;
 }
 
-test("La funzione getInitials restituisce le iniziali di un nome completo.", () =>  {
-    expect(getInitials("Mario Rossi")).toBe("MR");
-})
+test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
+  expect(getInitials("Mario Rossi")).toBe("MR");
+});
 
+// Snack 2
 function createSlug(stringa) {
-  return stringa.toLowerCase()
+  return stringa.toLowerCase().replaceAll(" ", "-");
 }
 
 test("La funzione createSlug restituisce una stringa in lowercase.", () => {
-    expect(createSlug("SONO SCRITTO IN MAIUSCOLO")).toBe("sono scritto in maiuscolo");
-})
+  expect(createSlug("SONO SCRITTO IN MAIUSCOLO")).toBe(
+    "sono-scritto-in-maiuscolo"
+  );
+});
 
+// Snack 3
 function average(numeri) {
-  const somma = numeri.reduce((acc, val) => acc + val, 0);
-  return somma / numeri.length;
+  return (somma = numeri.reduce((acc, val) => acc + val, 0) / numeri.length);
 }
-
 
 test("La funzione average calcola la media aritmetica di un array di numeri", () => {
   expect(average([100, 0])).toBe(50);
+});
+
+// Snack 4
+test("La funzione createSlug sostituisce gli spazi con -.", () => {
+  expect(createSlug("questo è un test")).toBe("questo-è-un-test");
 });
